d3dde29988e47b4e1eb152a242f3debf80443292
matuskonig <matuskonig@gmail.com>
2021-08-13 16:13:25 +0200

    removed friends property

--- Entities/AuthDto/UserDto.cs
+++ Entities/AuthDto/UserDto.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Entities.Enums;
 
 namespace Entities.AuthDto
 {
@@ -10,20 +8,5 @@ namespace Entities.AuthDto
         public string UserName { get; set; }
         public IEnumerable<FriendRequestDto> IncomingRequests { get; set; }
         public IEnumerable<FriendRequestDto> SentRequests { get; set; }
-
-        public IEnumerable<UserDto> Friends
-        {
-            get
-            {
-                var incoming = IncomingRequests?
-                    .Where(request => request.State == FriendRequestState.Accepted)
-                    .Select(request => request.From) ?? Enumerable.Empty<UserDto>();
-                var sent = SentRequests?
-                    .Where(request => request.State == FriendRequestState.Accepted)
-                    .Select(request => request.To) ?? Enumerable.Empty<UserDto>();
-                ;
-                return incoming.Concat(sent);
-            }
-        }
     }
 }
