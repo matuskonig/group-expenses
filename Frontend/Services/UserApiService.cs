03706ba3a6a46c5b693776ee330164cd86f08e2b
matuskonig <matuskonig@gmail.com>
2021-08-13 16:13:46 +0200

    refactor, bug fixes

--- Frontend/Services/UserApiService.cs
+++ Frontend/Services/UserApiService.cs
@@ -14,15 +14,14 @@ namespace Frontend.Services
     {
         private readonly HttpClient httpClient;
 
-
-        private UserDto _currentUser;
+        private UserDto currentUser;
 
         public UserDto CurrentUser
         {
-            get => _currentUser;
-            set
+            get => currentUser;
+            private set
             {
-                _currentUser = value;
+                currentUser = value;
                 OnChange?.Invoke();
             }
         }
@@ -34,48 +33,58 @@ namespace Frontend.Services
             this.httpClient = httpClient;
         }
 
-        public async void LoadCurrent()
+        public async Task LoadCurrent()
         {
             var result = await httpClient.GetFromJsonAsync<UserDto>("user/current");
-            CurrentUser ??= result;
+            CurrentUser = result ?? CurrentUser;
         }
 
-        public async void SendNewFriendRequest(UserDto user)
+        public async Task SendNewFriendRequest(UserDto user)
         {
-            var url = $"user/sendNewRequest/{user.Id}";
-            var result = await httpClient.GetAsync(url);
+            var result = await httpClient.GetAsync($"user/sendNewRequest/{user.Id}");
             if (result.StatusCode != HttpStatusCode.OK)
-                throw new ArgumentException("wrong user");
+                throw new ArgumentException("unable to send friend request");
         }
 
-        public async void AcceptFriendRequest(FriendRequestDto request)
+        public async Task AcceptFriendRequest(FriendRequestDto request)
         {
             var response = await httpClient.GetAsync($"user/acceptRequest/{request.Id}");
             if (!response.IsSuccessStatusCode)
                 throw new ArgumentException("bad sth");
             var friendRequest = await response.Content.ReadFromJsonAsync<FriendRequestDto>();
             UpdateIncomingFriendRequest(friendRequest);
+            UpdateSendFriendRequest(friendRequest);
+            OnChange?.Invoke();
         }
 
-        public async void RejectFriendRequest(FriendRequestDto request)
+        public async Task RejectFriendRequest(FriendRequestDto request)
         {
             var response = await httpClient.GetAsync($"user/rejectRequest/{request.Id}");
             if (!response.IsSuccessStatusCode)
                 throw new ArgumentException("bad sth");
             var friendRequest = await response.Content.ReadFromJsonAsync<FriendRequestDto>();
             UpdateIncomingFriendRequest(friendRequest);
-        }
-
-        private void UpdateIncomingFriendRequest(FriendRequestDto friendRequest)
-        {
-            CurrentUser.IncomingRequests = CurrentUser.IncomingRequests.Select(incomingRequest =>
-                incomingRequest.Id == friendRequest?.Id
-                    ? friendRequest
-                    : incomingRequest);
+            UpdateSendFriendRequest(friendRequest);
             OnChange?.Invoke();
         }
 
-        public async Task<SearchUserResponse> SearchUsers(SearchUserRequest request)
+        private void UpdateIncomingFriendRequest(FriendRequestDto updatedFriendRequest)
+        {
+            CurrentUser.IncomingRequests = CurrentUser.IncomingRequests.Select(incomingRequest =>
+                incomingRequest.Id == updatedFriendRequest?.Id
+                    ? updatedFriendRequest
+                    : incomingRequest).ToArray();
+        }
+
+        private void UpdateSendFriendRequest(FriendRequestDto updatedFriendRequest)
+        {
+            CurrentUser.SentRequests = CurrentUser.SentRequests.Select(sentRequest =>
+                sentRequest.Id == updatedFriendRequest?.Id
+                    ? updatedFriendRequest
+                    : sentRequest).ToArray();
+        }
+
+        private async Task<SearchUserResponse> SearchUsers(SearchUserRequest request)
         {
             var response = await httpClient.PostAsJsonAsync("user/searchUser", request);
             var data = await response.Content.ReadFromJsonAsync<SearchUserResponse>();
@@ -85,7 +94,7 @@ namespace Frontend.Services
         public async Task<IEnumerable<UserDto>> SearchFriendByName(string username)
         {
             var data = await SearchUsers(new SearchUserRequest {UserName = username});
-            return data.UsersByUserName ;
+            return data.UsersByUserName;
         }
     }
 }
