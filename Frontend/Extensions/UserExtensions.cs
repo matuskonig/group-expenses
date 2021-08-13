aeee9731622b330b776b44cac17501452a1a5a8c
matuskonig <matuskonig@gmail.com>
2021-08-13 16:14:48 +0200

    added extensions

---
+++ Frontend/Extensions/UserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities.AuthDto;
+using Entities.Enums;
+
+namespace Frontend.Extensions
+{
+    public static class UserExtensions
+    {
+        public static IEnumerable<UserDto> Friends(this UserDto currentUser)
+        {
+            var incoming = currentUser.IncomingRequests?
+                .Where(request => request.State == FriendRequestState.Accepted)
+                .Select(request => request.From) ?? Enumerable.Empty<UserDto>();
+            var sent = currentUser.SentRequests?
+                .Where(request => request.State == FriendRequestState.Accepted)
+                .Select(request => request.To) ?? Enumerable.Empty<UserDto>();
+            return incoming.Concat(sent);
+        }
+    }
+}
