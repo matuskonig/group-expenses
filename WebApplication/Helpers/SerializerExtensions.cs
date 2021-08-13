11a62f443da85f77370c88ce338102508b74b7e2
matuskonig <matuskonig@gmail.com>
2021-08-13 16:12:03 +0200

    added serialization

--- WebApplication/Helpers/SerializerExtensions.cs
+++ WebApplication/Helpers/SerializerExtensions.cs
@@ -7,14 +7,14 @@ namespace WebApplication.Helpers
 {
     public static class SerializerExtensions
     {
-        public static UserDto Serialize(this ApplicationUser user)
+        public static UserDto Serialize(this ApplicationUser user, bool serializeRequests = true)
         {
             return new()
             {
                 Id = user.Id,
                 UserName = user.UserName,
-                IncomingRequests = user.IncomingRequests?.Select(Serialize),
-                SentRequests = user.SentRequests?.Select(Serialize)
+                IncomingRequests = serializeRequests ? user.IncomingRequests?.Select(Serialize): null,
+                SentRequests = serializeRequests ? user.SentRequests?.Select(Serialize) : null
             };
         }
 
@@ -26,8 +26,8 @@ namespace WebApplication.Helpers
                 Created = friendshipStatus.Created,
                 Modified = friendshipStatus.Modified,
                 State = friendshipStatus.State,
-                From = friendshipStatus.From?.Serialize(),
-                To = friendshipStatus.To?.Serialize()
+                From = friendshipStatus.From?.Serialize(serializeRequests: false),
+                To = friendshipStatus.To?.Serialize(serializeRequests: false)
             };
         }
     }
