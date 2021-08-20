3ac692b6040913bc28076f098e7e6fa870b0d5e3
matuskonig <matuskonig@gmail.com>
2021-08-20 21:59:02 +0200

    added groupService

---
+++ Frontend/Services/GroupService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Entities.GroupDto;
+
+namespace Frontend.Services
+{
+    public class GroupService
+    {
+        private readonly HttpClient _httpClient;
+        private IEnumerable<SinglePurposeUserGroupDto> _userGroups = Enumerable.Empty<SinglePurposeUserGroupDto>();
+        public event Action OnChange;
+
+        public GroupService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IEnumerable<SinglePurposeUserGroupDto> UserGroups
+        {
+            get => _userGroups;
+            set
+            {
+                _userGroups = value;
+                OnChange?.Invoke();
+            }
+        }
+
+        public async Task LoadAll()
+        {
+            var data =
+                await _httpClient.GetFromJsonAsync<IEnumerable<SinglePurposeUserGroupDto>>("group/currentUserGroup");
+            if (data != null)
+            {
+                UserGroups = data;
+            }
+        }
+
+        public async Task<SinglePurposeUserGroupDto> AddNewGroup(string groupName)
+        {
+            var response = await _httpClient.PostAsJsonAsync("group/addNewGroup",
+                new SinglePurposeUserGroupDto { Name = groupName });
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("sth went wrong");
+            var data = await response.Content.ReadFromJsonAsync<SinglePurposeUserGroupDto>();
+            return data;
+        }
+
+        public async Task<SinglePurposeUserGroupDto> ModifyUserGroup(SinglePurposeUserGroupDto group)
+        {
+            var response = await _httpClient.PostAsJsonAsync("group/modifyUserGroup", group);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("sth went wrong");
+            var data = await response.Content.ReadFromJsonAsync<SinglePurposeUserGroupDto>();
+            return data;
+        }
+
+        public async Task<UnidirectionalPaymentGroupDto> ModifyPaymentGroup(UnidirectionalPaymentGroupDto paymentGroup)
+        {
+            var response = await _httpClient.PostAsJsonAsync("group/modifyPaymentGroup", paymentGroup);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("sth went wrong");
+            var data = await response.Content.ReadFromJsonAsync<UnidirectionalPaymentGroupDto>();
+            return data;
+        }
+
+        public async Task<SinglePaymentDto> ModifySinglePayment(SinglePaymentDto payment)
+        {
+            var response = await _httpClient.PostAsJsonAsync("group/modifySinglePayment", payment);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("something is wrong");
+            var data = await response.Content.ReadFromJsonAsync<SinglePaymentDto>();
+            return data;
+        }
+    }
+}
