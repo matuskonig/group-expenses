5934093edc64b19186ec1af31a58ff0aebd6d467
matuskonig <matuskonig@gmail.com>
2021-08-21 17:29:13 +0200

    added property equality comparer

---
+++ WebApplication/Helpers/PropertyEqualityComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication.Helpers
+{
+    public class PropertyEqualityComparer<TBase, TProperty> : IEqualityComparer<TBase>
+    {
+        private readonly Func<TBase, TProperty> _getter;
+
+        public PropertyEqualityComparer(Func<TBase, TProperty> getter)
+        {
+            _getter = getter;
+        }
+
+        public bool Equals(TBase? x, TBase? y)
+        {
+            return EqualityComparer<TProperty>.Default.Equals(_getter(x), _getter(y));
+        }
+
+        public int GetHashCode(TBase obj)
+        {
+            return _getter(obj).GetHashCode();
+        }
+    }
+}
