a29348f45502f4063ecf1fa200beba2f77e1a47b
matuskonig <matuskonig@gmail.com>
2021-08-21 17:31:18 +0200

    workspace fix

--- Frontend/Helpers/PropertyEqualityComparer.cs
+++ Frontend/Helpers/PropertyEqualityComparer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace WebApplication.Helpers
+namespace Frontend.Helpers
 {
     public class PropertyEqualityComparer<TBase, TProperty> : IEqualityComparer<TBase>
     {
