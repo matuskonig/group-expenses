f705c159bc060f605e8c599150dc386d41049a7b
matuskonig <matuskonig@gmail.com>
2021-08-21 18:59:19 +0200

    extensions

---
+++ Frontend/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Frontend.Helpers
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient httpClient,string requestUri, T value)
+        {
+            var content = JsonContent.Create(value);
+            return httpClient.PatchAsync(requestUri, content);
+        }
+    }
+}
