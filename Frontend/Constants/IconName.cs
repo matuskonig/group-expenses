55847e5f60c68038fef71779a5ca7aac867ebb74
matuskonig <matuskonig@gmail.com>
2021-08-29 19:42:47 +0200

    UI improvements

---
+++ Frontend/Constants/IconName.cs
@@ -0,0 +1,24 @@
+﻿namespace Frontend.Constants
+{
+    public readonly struct EnumValue<T>
+    {
+        private T Value { get; }
+
+        public EnumValue(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator T(EnumValue<T> enumValue)
+        {
+            return enumValue.Value;
+        }
+    }
+
+    public static class IconName
+    {
+        public static EnumValue<string> Plus = new("oi-plus");
+        public static EnumValue<string> Pencil = new("oi-pencil");
+        public static EnumValue<string> Trash = new("oi-trash");
+    }
+}
