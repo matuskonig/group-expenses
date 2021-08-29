f462997683a87500687c589cd88dca9474c97462
matuskonig <matuskonig@gmail.com>
2021-08-29 19:48:37 +0200

    added group name migration

---
+++ WebApplication/Migrations/20210829174708_AddedGroupName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class AddedGroupName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PaymentGroups",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PaymentGroups");
+        }
+    }
+}
