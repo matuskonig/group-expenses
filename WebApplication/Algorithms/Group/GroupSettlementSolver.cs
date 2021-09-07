b933346a7288d64bfb7025990b34d733df5c69d0
matuskonig <matuskonig@gmail.com>
2021-09-07 16:59:43 +0200

    redone group settlement algo

--- WebApplication/Algorithms/Group/GroupSettlementSolver.cs
+++ WebApplication/Algorithms/Group/GroupSettlementSolver.cs
@@ -1,54 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using WebApplication.Authentication;
 using WebApplication.Models;
 
 namespace WebApplication.Algorithms.Group
 {
     public static class GroupSolver
     {
-        public static IEnumerable<SinglePaymentRecord> FindGroupSettlement(
-            SinglePurposeUserGroup group)
+        public static IEnumerable<SinglePaymentRecord> FindGroupSettlement(SinglePurposeUserGroup group)
         {
-            var paymentGraphEdges = group
-                .GetGroupRecords()
-                .FixWrongInput()
-                .MergePaymentsInOneDirection()
-                .MergePaymentsInOpposingDirection()
-                .ToList();
-
-            var userPaidBalance = paymentGraphEdges
-                .GroupBy(payment => payment.PaymentBy)
-                .Select(grouping => (User: grouping.Key, Balance: -grouping.Sum(paymentRecord => paymentRecord.Price)));
-            var userReceivedBalance = paymentGraphEdges
-                .GroupBy(payment => payment.PaymentFor)
-                .Select(grouping => (User: grouping.Key, Balance: grouping.Sum(paymentRecord => paymentRecord.Price)));
-            var totalBalance = userPaidBalance
-                .Concat(userReceivedBalance)
-                .GroupBy(userBalanceTuple => userBalanceTuple.User)
-                .ToDictionary(grouping => grouping.Key,
-                    grouping => grouping.Sum(userBalanceTuple => userBalanceTuple.Balance));
+            var userBalances = group.GetGroupRecords().GetUserBalances();
 
             var result = new List<SinglePaymentRecord>();
-            while (totalBalance.Any(pair => pair.Value != decimal.Zero))
+            while (userBalances.Any(pair => pair.Value != decimal.Zero))
             {
                 /*
-                 * Do while you have verticies with nonzero balance
+                 * Do while you have vertices with nonzero balance
                  * 1. Find a user who paid most and user who receiver most
                  * 2. Do a payment from the user, who received the most to the user, which paid most - value the user is missing
                  * 3. You have successfully removed one user from the graph
                  */
-                var minValue = totalBalance.Min(pair => pair.Value);
+                var minValue = userBalances.Min(pair => pair.Value);
                 if (minValue > 0)
                     throw new InvalidOperationException($"{nameof(minValue)} cannot be positive");
-                var maxValue = totalBalance.Max(pair => pair.Value);
+
+                var maxValue = userBalances.Max(pair => pair.Value);
                 if (maxValue < 0)
                     throw new InvalidOperationException($"{nameof(maxValue)} cannot be negative");
 
                 // Biggest dept from fellow users
-                var (minValueUser, _) = totalBalance.FirstOrDefault(pair => pair.Value == minValue);
+                var (minValueUser, _) = userBalances.FirstOrDefault(pair => pair.Value == minValue);
                 // Highest borrow
-                var (maxValueUser, _) = totalBalance.FirstOrDefault(pair => pair.Value == maxValue);
+                var (maxValueUser, _) = userBalances.FirstOrDefault(pair => pair.Value == maxValue);
 
                 //This payment settles at least one user, thus the loop will end
                 var payment = new SinglePaymentRecord
@@ -58,8 +42,8 @@ namespace WebApplication.Algorithms.Group
                     Price = Math.Min(-minValue, maxValue)
                 };
 
-                totalBalance[minValueUser] += payment.Price;
-                totalBalance[maxValueUser] -= payment.Price;
+                userBalances[minValueUser] += payment.Price;
+                userBalances[maxValueUser] -= payment.Price;
                 result.Add(payment);
             }
 
@@ -78,57 +62,24 @@ namespace WebApplication.Algorithms.Group
                     })
                 );
         }
-
-        private static IEnumerable<SinglePaymentRecord> MergePaymentsInOneDirection(
-            this IEnumerable<SinglePaymentRecord> records)
+        
+        private static Dictionary<ApplicationUser, decimal> GetUserBalances(
+            this IEnumerable<SinglePaymentRecord> payments)
         {
-            return records
-                .GroupBy(singlePaymentRecord =>
-                    (From: singlePaymentRecord.PaymentBy, To: singlePaymentRecord.PaymentFor))
-                .Select(grouping =>
-                    new SinglePaymentRecord
-                    {
-                        PaymentBy = grouping.Key.From,
-                        PaymentFor = grouping.Key.To,
-                        Price = grouping.Sum(paymentRecord => paymentRecord.Price)
-                    });
-        }
+            var userBalances = new Dictionary<ApplicationUser, decimal>();
+            foreach (var payment in payments)
+            {
+                if (!userBalances.ContainsKey(payment.PaymentBy))
+                    userBalances.TryAdd(payment.PaymentBy, 0);
 
-        private static IEnumerable<SinglePaymentRecord> FixWrongInput(
-            this IEnumerable<SinglePaymentRecord> records)
-        {
-            return records
-                .Select(record => record.Price switch
-                {
-                    <0 => record with
-                    {
-                        PaymentFor = record.PaymentBy,
-                        PaymentBy = record.PaymentFor,
-                        Price = -record.Price
-                    },
-                    0 => null,
-                    _ => record
-                })
-                .Where(record => record != null);
-        }
+                if (!userBalances.ContainsKey(payment.PaymentFor))
+                    userBalances.TryAdd(payment.PaymentFor, 0);
 
-        private static IEnumerable<SinglePaymentRecord> MergePaymentsInOpposingDirection(
-            this IEnumerable<SinglePaymentRecord> records)
-        {
-            var paymentRecordsSource = records.ToDictionary(record => (record.PaymentBy, record.PaymentFor));
-            return paymentRecordsSource.Select(pair =>
-                {
-                    var ((paymentBy, paymentFor), paymentRecord) = pair;
-                    // If there exist no opposite edge -> return the single route
-                    if (!paymentRecordsSource.TryGetValue((paymentFor, paymentBy), out var otherPaymentRecord))
-                        return paymentRecord;
+                userBalances[payment.PaymentBy] -= payment.Price;
+                userBalances[payment.PaymentFor] += payment.Price;
+            }
 
-                    var difference = paymentRecord.Price - otherPaymentRecord.Price;
-                    return difference > 0
-                        ? paymentRecord with { Price = difference }
-                        : null;
-                })
-                .Where(paymentRecord => paymentRecord != null);
+            return userBalances;
         }
     }
 }
